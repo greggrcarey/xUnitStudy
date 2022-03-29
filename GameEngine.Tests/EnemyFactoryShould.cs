c0d353af8ce9350f09fff6841e35eae7fd372cf9
Gregg Carey <gregg.carey@outlook.com>
2022-03-29 09:33:24 -0500

    More Assert Method examples

---
+++ GameEngine.Tests/EnemyFactoryShould.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class EnemyFactoryShould
+    {
+        [Fact]
+        public void CreateNormalByDefault()
+        {
+            EnemyFactory sut = new();
+            Enemy enemy = sut.Create("Zombie");
+
+            Assert.IsType<NormalEnemy>(enemy);
+        }
+        [Fact]
+        public void CreateNormalByDefault_NotTypeExample()
+        {
+            EnemyFactory sut = new();
+            Enemy enemy = sut.Create("Zombie");
+
+            Assert.IsNotType<BossEnemy>(enemy);
+        }
+        [Fact]
+        public void CreateBossEnemy()
+        {
+            EnemyFactory sut = new();
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            Assert.IsType<BossEnemy>(enemy);
+        }
+        [Fact]
+        public void CreateBossEnemy_CastReturnedTypeExample()
+        {
+            EnemyFactory sut = new();
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            //Assert and get the cast result
+            BossEnemy boss = Assert.IsType<BossEnemy>(enemy);
+
+            //Additional asserts on the types object
+            Assert.Equal("Zombie King", boss.Name);
+        }
+        [Fact]
+        public void CreateBossEnemy_AssertAssignableTypes()
+        {
+            EnemyFactory sut = new();
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            //IsType is a strict check so this check fails
+            //Assert.IsType<Enemy>(enemy);
+
+            //This Assert check the inheritance chain
+            Assert.IsAssignableFrom<Enemy>(enemy);
+            
+        }
+        [Fact]
+        public void CreateSeperateInstances()
+        {
+            EnemyFactory sut = new();
+
+            Enemy enemy1 = sut.Create("Zombie");
+            Enemy enemy2 = sut.Create("Zombie");
+
+            Assert.NotSame(enemy1, enemy2);
+
+        }
+    }
+}
--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -81,6 +81,69 @@ namespace GameEngine.Tests
             Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
 
         }
+        [Fact]
+        public void StartWithDefaultHealth()
+        {
+            var sut = new PlayerCharacter();
+            Assert.Equal(100, sut.Health);
+        }
+
+        //Numberic Tests
+        [Fact]
+        public void IncreaseHealthAffterSleeping()
+        {
+            PlayerCharacter sut = new();
+            sut.Sleep();
+
+            //You can add a conditional expression to Assert.Equals to test numeric values, but the Test Failures will not be expressive or helpful
+            Assert.InRange(sut.Health, 101, 200);
+        }
+        [Fact]
+        public void NotHaveNickNameByDefault()
+        {
+            PlayerCharacter sut = new();
+
+            Assert.Null(sut.Nickname); //There is also a NotNull method
+        }
+        [Fact]
+        public void HaveALongBow()
+        {
+            PlayerCharacter sut = new();
+            Assert.Contains("Long Bow", sut.Weapons);
+        }
+        [Fact]
+        public void NotHaveAStaffOfWonder()
+        {
+            PlayerCharacter sut = new();
+            Assert.DoesNotContain("Staff Of Wonder", sut.Weapons);
+        }
+        [Fact]
+        public void HaveAtLeastOneKindOfSword()
+        {
+            PlayerCharacter sut = new();
+            Assert.Contains(sut.Weapons, weapon => weapon.Contains("Sword"));//Assert against a predicate
+        }
+
+        [Fact]
+        public void HaveAllExpectedWeapons()
+        {
+            PlayerCharacter sut = new();
+
+            var expectedWeapons = new[]
+            {
+                "Long Bow",
+                "Short Bow",
+                "Short Sword"
+            };
+
+            Assert.Equal(expectedWeapons, sut.Weapons);
+        }
+        [Fact]
+        public void HaveNoEmptyDefaultWeapons()
+        {
+            PlayerCharacter sut = new();
+            Assert.All(sut.Weapons, weapon => Assert.False(string.IsNullOrWhiteSpace(weapon)));//Asserts against each weapon in the collection
+        }
 
 
     }
