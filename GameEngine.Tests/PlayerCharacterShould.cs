036d5e47a6d75ccadd1b1726c0e3df61aa8770d8
Gregg Carey <gregg.carey@outlook.com>
2022-03-29 15:30:38 -0500

    Single private instance PlayerCharacter for test, added IDispose

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -1,24 +1,39 @@
 using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace GameEngine.Tests
 {
-    public class PlayerCharacterShould
+    public class PlayerCharacterShould : IDisposable
     {
+        private readonly PlayerCharacter sut;
+        private readonly ITestOutputHelper output;
+
+        public PlayerCharacterShould(ITestOutputHelper output)
+        {
+           
+            this.output = output;
+
+            this.output.WriteLine("Creating new PlayerCharacter");
+            sut = new PlayerCharacter();
+        }
+        public void Dispose() 
+        { 
+            output.WriteLine($"Disposing PlayerCharacter {sut.FullName}"); 
+            GC.SuppressFinalize(this);
+        }
+
         [Fact]
         public void BeInexperiencedWhenNew()
         {
-            PlayerCharacter sut = new PlayerCharacter(); //sut stands for System Under Test
+            
             Assert.True(sut.IsNoob); //Test a boolean
         }
         [Fact]
         public void CalculateFullName()
         {
-            PlayerCharacter sut = new PlayerCharacter
-            {
-                FirstName = "Sarah",
-                LastName = "Smith"
-            };
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
 
             Assert.Equal("Sarah Smith", sut.FullName);
 
@@ -26,33 +41,28 @@ namespace GameEngine.Tests
         [Fact]
         public void HaveFullNameStartingWithFirstName()
         {
-            PlayerCharacter sut = new PlayerCharacter
-            {
-                FirstName = "Sarah",
-                LastName = "Smith"
-            };
+            
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+            
 
             Assert.StartsWith("Sarah", sut.FullName);
         }
         [Fact]
         public void HaveFullNameEndsWithWithLastName()
         {
-            PlayerCharacter sut = new PlayerCharacter
-            {
-                FirstName = "Sarah",
-                LastName = "Smith"
-            };
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
 
             Assert.EndsWith("Smith", sut.FullName);
         }
         [Fact]
         public void CalculateFullName_IgnoreCase()
         {
-            PlayerCharacter sut = new PlayerCharacter
-            {
-                FirstName = "SARAH",
-                LastName = "SMITH"
-            };
+
+            sut.FirstName = "SARAH";
+            sut.LastName = "SMITH";
+            
 
             Assert.Equal("Sarah Smith", sut.FullName, ignoreCase: true);
 
@@ -60,11 +70,10 @@ namespace GameEngine.Tests
         [Fact]
         public void CalculateFullName_SubstringAssertExample()
         {
-            PlayerCharacter sut = new()
-            {
-                FirstName = "Sarah",
-                LastName = "Smith"
-            };
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+          
 
             Assert.Contains("ah Sm", sut.FullName);
 
@@ -72,11 +81,8 @@ namespace GameEngine.Tests
         [Fact]
         public void CalculateFullNameWithTitleCase()
         {
-            PlayerCharacter sut = new()
-            {
-                FirstName = "Sarah",
-                LastName = "Smith"
-            };
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
 
             Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
 
@@ -84,7 +90,7 @@ namespace GameEngine.Tests
         [Fact]
         public void StartWithDefaultHealth()
         {
-            var sut = new PlayerCharacter();
+          
             Assert.Equal(100, sut.Health);
         }
 
@@ -92,7 +98,7 @@ namespace GameEngine.Tests
         [Fact]
         public void IncreaseHealthAffterSleeping()
         {
-            PlayerCharacter sut = new();
+         
             sut.Sleep();
 
             //You can add a conditional expression to Assert.Equals to test numeric values, but the Test Failures will not be expressive or helpful
@@ -101,33 +107,32 @@ namespace GameEngine.Tests
         [Fact]
         public void NotHaveNickNameByDefault()
         {
-            PlayerCharacter sut = new();
+          
 
             Assert.Null(sut.Nickname); //There is also a NotNull method
         }
         [Fact]
         public void HaveALongBow()
         {
-            PlayerCharacter sut = new();
             Assert.Contains("Long Bow", sut.Weapons);
         }
         [Fact]
         public void NotHaveAStaffOfWonder()
         {
-            PlayerCharacter sut = new();
+          
             Assert.DoesNotContain("Staff Of Wonder", sut.Weapons);
         }
         [Fact]
         public void HaveAtLeastOneKindOfSword()
         {
-            PlayerCharacter sut = new();
+          
             Assert.Contains(sut.Weapons, weapon => weapon.Contains("Sword"));//Assert against a predicate
         }
 
         [Fact]
         public void HaveAllExpectedWeapons()
         {
-            PlayerCharacter sut = new();
+          
 
             var expectedWeapons = new[]
             {
@@ -141,13 +146,13 @@ namespace GameEngine.Tests
         [Fact]
         public void HaveNoEmptyDefaultWeapons()
         {
-            PlayerCharacter sut = new();
+        
             Assert.All(sut.Weapons, weapon => Assert.False(string.IsNullOrWhiteSpace(weapon)));//Asserts against each weapon in the collection
         }
         [Fact]
         public void RaiseSleptEvent()
         {
-            PlayerCharacter sut = new();
+           
 
             Assert.Raises<EventArgs>(
                 handler => sut.PlayerSlept += handler,
@@ -157,12 +162,12 @@ namespace GameEngine.Tests
         [Fact]
         public void RaisePropertyChangedEvent()
         {
-            PlayerCharacter sut = new();
+         
 
             //Use the .PropertyChanged to test the INotifyPropertyChanged Interface of PlayerCharacter
             Assert.PropertyChanged(sut, "Health", () => sut.TakeDamage(10));
         }
 
-
+       
     }
 }
