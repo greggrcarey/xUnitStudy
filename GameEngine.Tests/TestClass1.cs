07f828b38bf0244a10bf16e81f7dccb55fba4b96
Gregg Carey <gregg.carey@outlook.com>
2022-03-30 09:11:34 -0500

    Share fixtures across Test Classes example

---
+++ GameEngine.Tests/TestClass1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    [Collection("GameState collection")]//No other Implementation, Add Attribute only
+    [Trait("Collection", "GameState Collection Example")]
+    public class TestClass1
+    {
+        private readonly GameStateFixture gameStateFixture;
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public TestClass1(GameStateFixture gameStateFixture, ITestOutputHelper testOutputHelper)
+        {
+            this.gameStateFixture = gameStateFixture;   
+            this.testOutputHelper = testOutputHelper;  
+        }
+        [Fact]
+        public void Test1()
+        {
+            testOutputHelper.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+        [Fact]
+        public void Test2()
+        {
+            testOutputHelper.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+
+    }
+}
---
+++ GameEngine.Tests/TestClass2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    [Collection("GameState collection")]//No other Implementation, Add Attribute only
+    [Trait("Collection","GameState Collection Example")]
+    public class TestClass2
+    {
+        private readonly GameStateFixture gameStateFixture;
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public TestClass2(GameStateFixture gameStateFixture, ITestOutputHelper testOutputHelper)
+        {
+            this.gameStateFixture = gameStateFixture;   
+            this.testOutputHelper = testOutputHelper;  
+        }
+        [Fact]
+        public void Test3()
+        {
+            testOutputHelper.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+        [Fact]
+        public void Test4()
+        {
+            testOutputHelper.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+
+    }
+}
